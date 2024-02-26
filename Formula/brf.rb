913e756f875ecbefbd2bfec6953f7da05945bc97
Geethadevi91 <mailme.geethabalachandran@gmail.com>
2024-02-26 22:16:40 +0800

    issue in sha was fixed

--- Formula/brf.rb
+++ Formula/brf.rb
@@ -2,7 +2,7 @@ class Brf < Formula
     desc "This tool search the regex pattern in the file"
     homepage "https://github.com/mailmebalachandran/homebrew-brf"
     url "https://github.com/mailmebalachandran/homebrew-brf/archive/v0.0.1.tar.gz"
-    sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+    sha256 "a47be015edfb3229a0af6f4fe8831c3d26ccd57523ca2fbd327644325866542f"
     license "MIT OR Apache-2.0"
   
     depends_on "rust" => :build
